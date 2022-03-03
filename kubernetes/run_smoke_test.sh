2185e7a627a884617ab6f90ddb666b7863f75d40
erickoch3 <ericwkoch@gmail.com>
2022-03-02 22:42:40 -0500

    too fast

--- kubernetes/run_smoke_test.sh
+++ kubernetes/run_smoke_test.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 TEST_URL="a00d1e37d2cf14c77a6ce2447da47529-1852105403.us-east-1.elb.amazonaws.com"
+sleep 15
 if curl "$TEST_URL" --max-time 1 | grep "Chinese"
   then
     echo "Success"
